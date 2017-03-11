87dd7c201330779041f63ed913b3885e57e4970e
arman-bd <arman@learnfie.com>
2017-03-11 18:33:16 +0600

    Multiplication Cross-Table

---
+++ multiplication-cross-table.c
@@ -0,0 +1,29 @@
+/*
+This program will print a multiplication cross table for 1 to 9.
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    printf(" N | 1  2  3  4  5  6  7  8  9  10");
+    printf("\n----------------------------------");
+    for(i = 1; i < 10; i++){
+        printf("\n %d |", i);
+        for(j = 1; j <= 10; j++){
+            k = i * j;
+
+            // For Proper Spacing
+            if(k > 0 && k < 10){printf(" %d ", k);}
+            if(k > 9 && k < 99){printf(" %d", k);}
+            if(k > 99){printf("%d", k);}
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
